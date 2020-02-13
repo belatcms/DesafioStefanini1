ec4d3305621a5ac050466597d3b261ac4741b369
dangodinho98 <dangodinho98@gmail.com>
2020-02-13 13:21:09 -0300

    Commit

---
+++ DesafioStefanini2/DesafioStefanini2/Detail.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DesafioStefanini2.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DesafioStefanini2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Detail : ContentPage
+    {
+        private const string Url = "http://api.openweathermap.org/data/2.5/weather?id={0}&appid={1}&lang=pt&units=metric";
+        private readonly HttpClient _client = new HttpClient();
+
+        private ObservableCollection<Datum> _cidades;
+
+        public Detail(int Id)
+        {
+            InitializeComponent();
+        }
+        public Detail()
+        {
+            InitializeComponent();
+        }
+        //Alimentando o listView
+        async override protected void OnAppearing()
+        {
+            base.OnAppearing();
+            //_cidades = new ObservableCollection<Datum>(Id);
+            //DetalheCidades.ItemsSource = _cidades;
+            base.OnAppearing();
+        }
+    }
+}
---
+++ DesafioStefanini2/DesafioStefanini2/Model/Cidade.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioStefanini2.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Cidade
+    {
+        [JsonProperty("data")]
+        public List<Datum> Data { get; set; }
+    }
+
+    public partial class Datum
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("coord")]
+        public Coord Coord { get; set; }
+
+        [JsonProperty("country")]
+        public Country Country { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("zoom")]
+        public long Zoom { get; set; }
+    }
+
+    public partial class Coord
+    {
+        [JsonProperty("lon")]
+        public double Lon { get; set; }
+
+        [JsonProperty("lat")]
+        public double Lat { get; set; }
+    }
+
+    public enum Country { Br };
+}
